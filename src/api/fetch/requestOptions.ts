b47a46e54f15434d68ebd99b9d80b90e87711222
Steven Lim <limhwsteven@gmail.com>
2022-06-26 20:20:19 +0700

    Update requestOptions.ts

--- src/api/fetch/requestOptions.ts
+++ src/api/fetch/requestOptions.ts
@@ -17,3 +17,23 @@ export const patchRequestOptions = {
     method: 'PATCH',
     credentials: 'include' as RequestCredentials,
 }
+
+export const abortableGetRequestOptions = (signal: AbortSignal) => ({
+    ...getRequestOptions,
+    signal
+})
+
+export const abortablePostRequestOptions = (signal: AbortSignal) => ({
+    ...postRequestOptions,
+    signal
+})
+
+export const abortableDeleteRequestOptions = (signal: AbortSignal) => ({
+    ...deleteRequestOptions,
+    signal
+})
+
+export const abortablePatchRequestOptions = (signal: AbortSignal) => ({
+    ...patchRequestOptions,
+    signal
+})
