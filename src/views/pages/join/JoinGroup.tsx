de49ff9e84c83be17c482f59453179613093543b
Steven Lim <limhwsteven@gmail.com>
2022-07-24 21:59:17 +0800

    Add testing for requests

--- src/views/pages/join/JoinGroup.tsx
+++ src/views/pages/join/JoinGroup.tsx
@@ -12,6 +12,7 @@ import Match from "./match/Match";
 import AlertDismissible from "../../components/form/AlertDismissible";
 // import ProviderSettings from "./ProviderSettings";
 import CounselRequests from "../admin/CounselRequests";
+import JoinGroupRequests from "./JoinGroupRequests";
 
 const JoinGroup = () => {
     const { details } = useSelector((state: any) => state.user);
@@ -26,6 +27,7 @@ const JoinGroup = () => {
         fetch(config.API_URL + "/join", abortableGetRequestOptions(abortController.signal))
             .then(response => response.json())
             .then(data => {
+                console.log(data)
                 setRequests(data);
             })
             .catch(err => console.log(err));
@@ -45,39 +47,6 @@ const JoinGroup = () => {
             .then(data => {
                 console.log(data);
             });
-        window.location.reload();
-    }
-
-    const handleApprove = async (requestID : number) => {
-        const requestOptions = {
-            ...patchRequestOptions,
-            body: JSON.stringify({
-                "approve": true
-            })
-        }
-        await fetch(config.API_URL + "/join/" + requestID, requestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data));
-        window.location.reload();
-    }
-
-    const handleReject = async (requestID : number) => {
-        const requestOptions = {
-            ...patchRequestOptions,
-            body: JSON.stringify({
-                "approve": false
-            })
-        }
-        await fetch(config.API_URL + "/join/" + requestID, requestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data));
-        window.location.reload();
-    }
-
-    const handleDelete = async (requestID : string) => {
-        await fetch(config.API_URL + "/join/" + requestID, deleteRequestOptions)
-            .then(response => response.json())
-            .then(data => console.log(data));
         window.location.reload();
     }
 
@@ -135,41 +104,7 @@ const JoinGroup = () => {
                             details.user_role === "MEMBER" &&
                             <div>
                             <h2>Group Requests</h2>
-                                <Table className="joinGroup_table" size="lg" width={100} hover>
-                                    <thead>
-                                        <tr className="">
-                                            <th className="display-none">Request ID</th>
-                                            <th>Applicant</th>
-                                            <th>Group#ID</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {requests.map((request, id) => {
-                                            return (
-                                                <tr key={id} className="">
-                                                    <td className="display-none">{request.join_request.id}</td>
-                                                    {
-                                                        request.user.id === details.id
-                                                        ? <td>You</td>
-                                                        : <td>{request.user.first_name}</td>
-                                                    }
-                                                    <td>{request.group.group_name}#{request.join_request.group_id}</td>
-                                                    <td className="request_action_buttons">
-                                                        {
-                                                            request.user.email === details.email
-                                                            ? <Button onClick={() => handleDelete(request.join_request.id)} className="request_delete">Delete</Button>
-                                                            : <div>
-                                                                <Button onClick={() => handleApprove(request.join_request.id)} className="request_approve">Approve</Button>
-                                                                <Button onClick={() => handleReject(request.join_request.id)} className="request_reject">Reject</Button>
-                                                            </div>
-                                                        }
-                                                    </td>
-                                                </tr>
-                                            )
-                                        })}
-                                    </tbody>
-                                </Table>
+                                <JoinGroupRequests requests={requests} details={details} />
                             </div>
                         }
                 </div>
